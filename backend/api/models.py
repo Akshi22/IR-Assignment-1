b350addf3ddd234704574a47b991ec125852f94a
Akshi22 <69581478+Akshi22@users.noreply.github.com>
2020-10-21 21:29:00 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,9 @@ class Lyrics(models.Model):
     artist = models.CharField(max_length=200)
     song = models.CharField(max_length=200)
     lyrics = models.CharField(max_length=2048)
+    #model to be returned
 
 
 class Search(models.Model):
     keyword = models.CharField(max_length=200)
+    #model to take
